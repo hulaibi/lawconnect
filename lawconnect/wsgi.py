467dd071699e71e8c7d93be727a6bb15bc4de550
HasanNaser <hassan.alhulaibi@gmail.com>
2025-07-30 12:03:28 +0300

    Add dotenv fix for Render

--- lawconnect/wsgi.py
+++ lawconnect/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-import dotenv
-dotenv.load_dotenv()
+
+if os.path.exists('.env'):
+    import dotenv
+    dotenv.load_dotenv()
 
 from django.core.wsgi import get_wsgi_application
 
